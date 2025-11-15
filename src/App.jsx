ad61715f0e4dc0a83d5ca50b9b32eeb2a0f1c33a
Ciril Plackal <mail.cirilplackal@gmail.com>
2025-11-15 19:36:19 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-xl font-bold text-red-500">
+        Hello Tailwind CSS with React and Vite!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
